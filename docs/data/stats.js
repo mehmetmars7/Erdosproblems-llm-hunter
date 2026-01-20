ee7e4040838acdc35019e5c59a9fbd370fddcbfc
mehmetmars7 <mehmet@example.com>
2026-01-20 19:08:54 +0000

    Add known reviews and update 570 reviewers

--- docs/data/stats.js
+++ docs/data/stats.js
@@ -3,10 +3,10 @@ var siteStats = {
     "total_problems": 676,
     "with_attacks": 676,
     "models": [
+      "gpt pro 5.2",
       "claude opus 4.5",
       "gpt 5.2",
-      "codex 5.2 extra high",
-      "gpt pro 5.2"
+      "codex 5.2 extra high"
     ]
   },
   "mo": {
